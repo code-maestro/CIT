d83a123d99b144349ec910a356f0495b1c0dd9d0
code-maestro <michaelajnew@gmail.com>
2021-11-11 19:26:03 +0300

    ɑ - Includes the assignments

---
+++ day_two.py
@@ -0,0 +1,20 @@
+def summation():
+    return 1000 + 414
+
+print(summation())
+
+name = "mikael"
+print(name[1:])
+
+def hobbies(one, two):
+    print("These are the favorite shows: " + one + "\t" + two)
+
+hobbies("Binge watching", "reading")
+
+print("my name is %s" % "mikael")
+
+def toFloat():
+    return float(input("Enter a number: "))
+
+print(toFloat())
+
--- func.py
+++ func.py
@@ -1,9 +1,16 @@
-print(input("Name:"))
+import random
 
+y = "COMPUTERCLASS"
 
-number = 2
+print(y[8:11])
+print(y.index('S'))
+print(y.count('A'))
+print(y.count('S'))
+print(y.lower())
 
-def sum():
-    
-    
-    
+def sum_of(num1):
+    a = random.randint(1,20)
+    print(a)
+    return (a*num1)
+
+print(sum_of(4))
