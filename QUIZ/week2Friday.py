001e339d5b6201943b23e78eab80929ed5037b5d
code-maestro <michaelajnew@gmail.com>
2021-11-23 18:52:27 +0300

    week 3 commit

---
+++ QUIZ/week2Friday.py
@@ -0,0 +1,37 @@
+# # list that prints "mikael" multiple times
+# name_list = ["mikael", "ajuna"]
+
+# for i in range(10):
+#     for name in name_list:
+#         print(name)
+
+
+# for i in range(1, 20):
+#     print(i)
+
+# if statement that returns False
+
+
+# def func():
+#     if 5 > 2:
+#         return False;
+#     else:
+#         return True;
+
+# print(func())
+
+# for i in range(20):
+#     if i % 2 != 0:
+#         print(i)
+
+def func(num):
+    print(num)
+
+func(1021)
+
+list2d = [
+    [2, 3], 
+    [4, 6]
+]
+
+print(list2d[1][1])
