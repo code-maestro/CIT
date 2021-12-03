617a673f9dea3e71c65aeceadbdf56f2ff3f95a5
code-maestro <michaelajnew@gmail.com>
2021-12-03 16:55:26 +0300

    😒 Formats strings in test.py file

--- week_four/selectionsort.py
+++ week_four/selectionsort.py
@@ -1,36 +1,38 @@
-arr1 = [100,49,2,1,34,-5,36,72]
+# arr1 = [100,49,2,1,34,-5,36,72]
 
-def selectionsort(array):
-    for i in range(len(array)):
-        min_index = i
-        for j in range(i+1,len(array)):
-            if array[j] < array[min_index]:
-                min_index = j
-        array[i], array[min_index] = array[min_index], array[i]
-    return print(array)
+# def selectionsort(array):
+#     for i in range(len(array)):
+#         min_index = i
+#         for j in range(i+1,len(array)):
+#             if array[j] < array[min_index]:
+#                 min_index = j
+#         array[i], array[min_index] = array[min_index], array[i]
+#     return print(array)
 
-selectionsort(arr1)
+# selectionsort(arr1)
 
 
-# Selection sort in Python
-def selectionSort(array):
-    iterations = 1
-    size = len(array)
-    for step in range(size):
-        print(f"Selection sort {iterations} iterations: {array}")
-        min_idx = step
-        for i in range(step + 1, size):
-            # Sorting in Ascending order
-            # to sort in descending order, change > to < in this line
-            # select the minimum element in each loop
-            if array[i] < array[min_idx]:
-                min_idx = i
-        iterations += 1
-        array[step], array[min_idx] = array[min_idx], array[step]
+# # Selection sort in Python
+# def selectionSort(array):
+#     iterations = 1
+#     size = len(array)
+#     for step in range(size):
+#         print(f"Selection sort {iterations} iterations: {array}")
+#         min_idx = step
+#         for i in range(step + 1, size):
+#             # Sorting in Ascending order
+#             # to sort in descending order, change > to < in this line
+#             # select the minimum element in each loop
+#             if array[i] < array[min_idx]:
+#                 min_idx = i
+#         iterations += 1
+#         array[step], array[min_idx] = array[min_idx], array[step]
 
 data = [-2, 5, 0, 11, -9]
 
-selectionSort(data)
+# selectionSort(data)
 
-print('Sorted Array in Ascending Order:')
-print(data)
+# print('Sorted Array in Ascending Order:')
+# print(data)
+
+print(data.sort())
--- week_four/test.py
+++ week_four/test.py
@@ -1,33 +1,16 @@
-def bubbleSort(array):
-    """Use swapped variable to keep track of swapping
-    Best Case ==> O(n) Iterates arr once if sorted
-    Worst Case ==> 0(n^2)
-    """
-    iterations = 0
-    for i in range(len(array)):
-        print(f"Bubble sort {iterations} iterations: {array}")
-        # keep track of swapping
-        swapped = False
-        
-        # loop to compare array elements
-        for j in range(0, len(array) - i - 1):
-            # compare two adjacent elements
-            # Sort in Ascending order
-            # change > to < to sort in descending order
-            if array[j] > array[j + 1]:
-                # swapping occurs if elements
-                # are not in the intended order
-                array[j], array[j + 1] = array[j + 1], array[j]
-                swapped = True
-        iterations += 1
-        # no swapping means the array is already sorted
-        # so no need for further comparison
-        if not swapped:
-            break
+print("I have {} dollars and im still {}".format(20,"broke"))
+#       0         1
+tup = ["Monday","Tuesday","Wednesday","Thursday","Friday"]
 
-data = [-2, 45, 0, 11, -9]
+y = "Days of the week: {0} \nFirstDay: {1} {2} {3}".format(tup[0],tup[1],tup[2],tup[3])
 
-bubbleSort(data)
 
-print('Sorted Array in Ascending Order:')
-print(data)
+print(y)
+print("I have {} dollars and im still {}".format(20,"broke"))
+#       0         1
+tup = ["Monday","Tuesday","Wednesday","Thursday","Friday"]
+
+y = "Days of the week: {0} \nFirstDay: {1} {2} {3}".format(tup[0],tup[1],tup[2],tup[3])
+
+
+print(y)
