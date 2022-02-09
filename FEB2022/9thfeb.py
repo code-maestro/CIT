f5b4e2a30e1d1647d5e4f96b7ea692ffda052de2
code-maestro <michaelajnew@gmail.com>
2022-02-09 19:13:02 +0300

    wednesday 9th feb 2022

---
+++ FEB2022/9thfeb.py
@@ -0,0 +1,46 @@
+# MICHAEL AJUNA 9TH FEBUARY 2022 ASSIGNMENT
+
+# 1.Create a function using the else and elif statement to get your output.
+def fun():
+    number = input("Enter a number: ")
+    if int(number) % 2 == 0:
+        print("The number is even")
+    elif int(number) % 2 == 1:
+        print("The number is odd")
+    else:
+        print("The number is not a number")
+
+fun()
+
+# 2.Create a nesting dictionary
+dictionary = {
+    "name": "Michael",
+    "age": "21",
+    "address": {
+        "street": "Tab Road",
+        "city": "Kampala"
+    }
+}
+
+# 3.Create a whileloop that prints only odd numbers
+x = 0
+while x < 15:
+    if x % 2 != 0:
+        print(x)
+    x += 1
+
+# You need to make a function that converts a foot value to inches. (1 foot has 12 inches). Defind convert() function, that takes the foot value as an argument and outputs the inches.
+def convert(foot):
+    return print(foot, "in Inches = ", str(foot * 12), "Inches")
+
+convert(int(input("Enter foot value to convert inches: ")))
+
+# 5. Create a list and and find out the length of that list.
+import random as r
+
+random_number_list = []
+for i in range(r.randint(1, 10)):
+    random_number_list.append(r.randint(2,5))
+
+print(random_number_list)
+print(len(random_number_list))
