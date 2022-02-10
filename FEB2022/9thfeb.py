9b9767467180cf225406e8c2c1e41d03a5846686
code-maestro <michaelajnew@gmail.com>
2022-02-10 17:05:30 +0300

    housekeeping

--- FEB2022/9thfeb.py
+++ FEB2022/9thfeb.py
@@ -12,41 +12,41 @@
 
 # fun()
 
-# 2.Create a nesting dictionary
-dict = {
-    "name": "Michael",
-    "age": "21",
-    "address": {
-        "street": "Tab Road",
-        "city": "Kampala"
-    }
-}
+# # 2.Create a nesting dictionary
+# dict = {
+#     "name": "Michael",
+#     "age": "21",
+#     "address": {
+#         "street": "Tab Road",
+#         "city": "Kampala"
+#     }
+# }
 
-print(dict)
-for keys, values in dict.items():
-    print(keys, values)
+# print(dict)
+# for keys, values in dict.items():
+#     print(keys, values)
 
-    
-# 3.Create a whileloop that prints only odd numbers
-import random
-x = 0
-while x < random.randint(0, 20):
-    if x % 2 != 0:
-        print(x)
-    x += 1
 
-# You need to make a function that converts a foot value to inches. (1 foot has 12 inches). Defind convert() function, that takes the foot value as an argument and outputs the inches.
-def convert(foot):
-    return print(foot, "in Inches = ", str(foot * 12), "Inches")
+# # 3.Create a whileloop that prints only odd numbers
+# import random
+# x = 0
+# while x < random.randint(0, 20):
+#     if x % 2 != 0:
+#         print(x)
+#     x += 1
 
-convert(int(input("Enter foot value to convert inches: ")))
+# # You need to make a function that converts a foot value to inches. (1 foot has 12 inches). Defind convert() function, that takes the foot value as an argument and outputs the inches.
+# def convert(foot):
+#     return print(foot, "in Inches = ", str(foot * 12), "Inches")
 
-# 5. Create a list and and find out the length of that list.
-import random as r
+# convert(int(input("Enter foot value to convert inches: ")))
 
-random_number_list = []
-for i in range(r.randint(1, 10)):
-    random_number_list.append(r.randint(2,5))
+# # 5. Create a list and and find out the length of that list.
+# import random as r
 
-print(random_number_list)
-print(len(random_number_list))
+# random_number_list = []
+# for i in range(r.randint(1, 10)):
+#     random_number_list.append(r.randint(2,5))
+
+# print(random_number_list)
+# print(len(random_number_list))
