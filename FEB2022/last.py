4a159579177e541c2dac10db208039fe4a1ef317
code-maestro <michaelajnew@gmail.com>
2022-02-28 18:20:52 +0300

    adds matplotlib assignment

--- FEB2022/last.py
+++ FEB2022/last.py
@@ -23,3 +23,16 @@ def sort_algorithm():
 
 sort_algorithm()
 plt.show()
+
+
+# ASSIGNMENT
+# 1. Create a 6-D array and flatten it to 1-D array.
+
+
+
+# 2. Create a whileloop that prints only even numbers in a range of 20
+# but skips 16.
+# 3. Steph Curry has scored [22,55,43,31,38] points in 5 games. Using a
+# function, find out his total points average in 5 games.
+# 4. Create a list and convert it into a tuple.
+# 5.Create any sorting algorithm using matplotlib.(Use my resources if needed)
